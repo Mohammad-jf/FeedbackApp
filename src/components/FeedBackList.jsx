f5c674c0bda47450446c702d12cbb51ba137fe64
mohammad-jalifathi <m.h.jalifathi@gmail.com>
2022-04-30 01:01:09 +0430

    animate feedbacks

--- src/components/FeedBackList.jsx
+++ src/components/FeedBackList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FeedBackItem from './FeedBackItem';
+import { motion,AnimatePresence } from 'framer-motion';
 
 
 
@@ -10,17 +11,42 @@ const FeedBackList = ({feedBacks,deleteHandler}) => {
     if(!feedBacks || feedBacks.length ===0){
         return <p>there is no feedbaack</p>
     }
-  return (
+
+    return (
       <div className='feedback-list'>
-        {feedBacks.map((item)=>
-            <FeedBackItem 
-            deleteHandler={deleteHandler}
+        <AnimatePresence>
+            {feedBacks.map((item)=>
+            <motion.div 
             key={item.id}
-            item={item}
-          
-            />)}
-        </div>
+            initial={{opacity:0,}}
+            animate={{opacity:1,transition:{duration:1},}}
+            exit={{opacity:0}}>
+
+                  <FeedBackItem 
+                    deleteHandler={deleteHandler}
+                    key={item.id}
+                    item={item}
+                  />
+
+            </motion.div>
+            )}
+        </AnimatePresence>
+      </div>
   )
+  
+    
+
+  // return (
+  //     <div className='feedback-list'>
+  //       {feedBacks.map((item)=>
+  //           <FeedBackItem 
+  //           deleteHandler={deleteHandler}
+  //           key={item.id}
+  //           item={item}
+          
+  //           />)}
+  //       </div>
+  // )
   
 }
 
